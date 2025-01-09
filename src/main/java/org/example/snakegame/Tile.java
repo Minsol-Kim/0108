806cbbea05100287c721816bb0a80f3571ff9b1b
Minsol Kim <yhe071624@gmail.com>
2025-01-09 15:17:29 +0900

    0109 #6

--- src/main/java/org/example/snakegame/Tile.java
+++ src/main/java/org/example/snakegame/Tile.java
@@ -1,26 +1,49 @@
 package org.example.snakegame;
 
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
+
 @ToString
-@Setter
 
 public class Tile {
-    //private -> convention
+
     private int num;
     private String desc;
     private String type;
     private int moving;
 
-
     public Tile(int num) {
         this.num = num;
-
     }
 
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 
+    public void setType(String type) {
+        this.type = type;
+    }
 
+    public void setMoving(int moving) {
+        this.moving = moving;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getMoving() {
+        return moving;
+    }
 }
